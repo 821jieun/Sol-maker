a47dfd581dff35386e8ee533cd67bfdf77861979
jieun <821jieun@gmail.com>
2018-06-05 12:36:38 -0400

    reformat

--- backend/drawing/controller.drawing.js
+++ backend/drawing/controller.drawing.js
@@ -3,7 +3,11 @@ const jwt = require('jsonwebtoken');
 const config = require('../../config');
 
 exports.verifyToken = (req, res, next) => {
-  if (req.method === 'OPTIONS') { res.writeHead(200) res.end() return }
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200);
+    res.end();
+    return
+  }
   const token = req.headers.authorization || req.params.token;
 
   if (!token) {
